b38ff3825144a5502d170f3e2cfce1bafc0c654f
Melissa Lafranchise <pulchrit@gmail.com>
2024-01-16 09:52:28 -0800

    add basic routes

---
+++ pages/events/[id].vue
@@ -0,0 +1,12 @@
+<script setup>
+  // useNuxtApp (built-in composable)gives us access to Nuxt app runtime context globally
+  // available on client and server
+  const nuxt = useNuxtApp();
+  // const route = useRoute();
+  console.log(nuxt)
+</script>
+
+<template>
+  <!-- $route.params pulls the route object from the url -->
+  <div>event id: {{  $route.params.id }}</div>
+</template>
