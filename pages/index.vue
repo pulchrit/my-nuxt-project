9a4525c5aa84fe851ddb45f39c084e522d1b0e28
Melissa Lafranchise <pulchrit@gmail.com>
2024-01-16 14:09:04 -0800

    the very basics of creating plugins

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <script setup>
-  const { sayHello } = useUtils();
-  sayHello(); // will log hello to the console using the useUtils composable
+  // Composable version
+  // const { sayHello } = useUtils();
+  // sayHello(); // will log hello to the console using the useUtils composable
+
+  // plugin version
+  const { $sayHello } = useNuxtApp();
+  $sayHello("Melissa");
 </script>
 
 <template>
@@ -8,7 +13,6 @@
     <!-- get Vue component definition from icones.js.org, create a local component by pasting what you get 
      from the site, then use that icon as a regular component -->
     <!-- <IconsBell /> -->
-    <p>Our composable says {{ sayHello }}</p>
     <!-- use ~/assets/Lagoon_ssro900.jpg if you don't create an assets alias in next.config.js -->
     <!-- <img class="w-full" src="@/assets/Lagoon_ssro900.jpg" alt="a mostly pink nebula in space with several bright stars" /> -->
     <!-- access the file without processing by putting it in the public folder and using it from there  -->
---
+++ plugins/myPlugin.ts
@@ -0,0 +1,11 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  console.log(nuxtApp) // this is the nuxt app context that comes from useNuxtApp()
+  // make this plugin functionality available everywhere in my app
+  // this is what a plugin does!
+  // You use this plugin in your scripts by calling useNuxtApp!
+  return {
+    provide: {
+      sayHello: (msg: string) => console.log(`Hello ${msg}!`),
+    }
+  }
+});
