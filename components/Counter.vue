0fb75d94cfe78809a56ed63f088d2c3d214ad201
Melissa Lafranchise <pulchrit@gmail.com>
2024-01-17 07:03:20 -0800

    useState for simple global state mgmt, counter example

---
+++ components/Counter.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const counter = useCounter();
+</script>
+
+<template>
+  <div>
+    Counter: {{ counter }}
+    <button class="px-4 py-3 text-white bg-blue-500 hover:bg-blue-400 mx-3 my-3" @click="counter++">+</button>
+    <button class="px-4 py-3 text-white bg-blue-500 hover:bg-blue-400 mx-3 my-3" @click="counter--">-</button>
+  </div>
+   
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,33 @@
 <script setup>
-  // Composable version
+  // Composable example for sayHello
   // const { sayHello } = useUtils();
   // sayHello(); // will log hello to the console using the useUtils composable
 
-  // plugin version
-  const { $sayHello } = useNuxtApp();
-  $sayHello("Melissa");
+  // plugin example for sayHello
+  // const { $sayHello } = useNuxtApp();
+  // $sayHello("Melissa");
+
+  // counter example for global state (uses useState in composables/state.ts)
+  const counter = useCounter();
 </script>
 
 <template>
   <div class="flex flex-col flex-1 mx-auto Main">
+    <!-- add an icon as a local component -->
     <!-- get Vue component definition from icones.js.org, create a local component by pasting what you get 
      from the site, then use that icon as a regular component -->
     <!-- <IconsBell /> -->
+   
+   <!-- use processed images (assets) or purely static images (public)  -->
     <!-- use ~/assets/Lagoon_ssro900.jpg if you don't create an assets alias in next.config.js -->
     <!-- <img class="w-full" src="@/assets/Lagoon_ssro900.jpg" alt="a mostly pink nebula in space with several bright stars" /> -->
     <!-- access the file without processing by putting it in the public folder and using it from there  -->
     <!-- <img class="w-full" src="/Lagoon_ssro900.jpg" alt="a mostly pink nebula in space with several bright stars" /> -->
-    <ContentDoc />
+   
+    <!-- Nuxt content module -->
+    <!-- <ContentDoc /> -->
+
+    <!-- Counter example for Nuxt shared state from useState -->
+   <Counter />
 </div>
 </template>
