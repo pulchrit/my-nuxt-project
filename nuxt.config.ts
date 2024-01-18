7f0a1ac97b226d77de4cd2df403694f7a5a78992
Melissa Lafranchise <pulchrit@gmail.com>
2024-01-18 06:41:03 -0800

    add a few notes on nuxt.config.ts options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,24 +17,63 @@ export default defineNuxtConfig({
   //   },
   // },
   // auto import works for components and some other things
-  // we can add an alias
+  // we can add an alias or many to reduce the import path length
+  
+  // runtimeConfig is for anything sensitive, available server side-only
+  // unless under the .public property
+  // runtimeConfig: {
+  //   // The private keys which are only available server-side
+  //   apiSecret: '123',
+  //   // Keys within public are also exposed client-side
+  //   public: {
+  //     apiBase: '/api'
+  //   }
+  // },
   alias: {
     // '/' is the root of this project, allows dynamic imports, OLD WAY
-    // '@': resolve(__dirname, '/'), 
+    // '@': resolve(__dirname, '/'), this is now a default so you can just use @
     assets: "/<rootDir>/assets",
   },
-  // If you intall a nuxt module, you can configure it here!
+ 
+  // baseURL can be changed here
+ 
+  // buildAssetDir: "/_nuxt/" is the default, relative to baseUrl
+  
+  // buildDir: "/<rootDir>/.nuxt' is the defualt. Sometimes you need to change
+  // this if a tool assume .nuxt is a hidden directory
+  
+  // If you install a nuxt module, you can configure it here!
   modules: [
     '@nuxt/content',
     '@pinia/nuxt',
   ],
   css: ['~/assets/main.scss'],
+
+  // Add other extension that you want Nuxt to process, for instance graphQl
+  // extensions: [".graphql", ".gql"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  // You can also add your tailwind.css config here instead of in tailwind.config.js
+  // You can do this for other modules
+  // tailwindcss: {
+  //   content: [
+  //   './components/**/*.{js,vue,ts}',
+  //   './layouts/**/*.vue',
+  //   './pages/**/*.vue',
+  //   './plugins/**/*.{js,ts}',
+  //   './app.vue',
+  //   './error.vue',
+  // ],
+  // theme: {
+  //   extend: {},
+  // },
+  // plugins: [],
+  // },
   devtools: { enabled: true },
   // temporarily turning this to spa app (or static app, you need to do this too)
   // just to show that useFecth works for both client and server side rendering, ssr: false,
